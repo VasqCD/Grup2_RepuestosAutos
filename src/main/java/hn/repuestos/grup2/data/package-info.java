6a1665e7d0ab5e39e7c3e9e724fc650f86ac459a
start.vaadin.com <no-reply@vaadin.com>
2024-03-08 04:37:20 +0000

    Generated project

---
+++ src/main/java/hn/repuestos/grup2/data/package-info.java
@@ -0,0 +1,4 @@
+@NonNullApi
+package hn.repuestos.grup2.data;
+
+import org.springframework.lang.NonNullApi;
---
+++ src/main/java/hn/repuestos/grup2/services/package-info.java
@@ -0,0 +1,4 @@
+@NonNullApi
+package hn.repuestos.grup2.services;
+
+import org.springframework.lang.NonNullApi;
